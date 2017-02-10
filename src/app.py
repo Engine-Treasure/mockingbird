044bfa39c21b073fbb6589cdc0dd11b3a1bcdcb3
Engine Treasure <enginechen07@gmail.com>
2017-02-10 20:37:54 +0800

    chapter 3.2

--- src/app.py
+++ src/app.py
@@ -1,27 +1,25 @@
 # -*-coding: utf-8 -*-
 
 from flask.ext.script import Manager
-from flask import Flask, redirect, make_response
+from flask.ext.bootstrap import Bootstrap
+from flask import Flask, redirect, make_response, render_template
 
 app = Flask(__name__)  # Flask 用这个参数决定程序的根目录
 app.debug = True
 
-
+bootstrap = Bootstrap(app)
 manager = Manager(app)
 
 
 @app.route("/")
 def index():
-    return "<h1>Welcome to kissg's World!</h1>"
+    return render_template("index.html")
 
 
 # <name> 为 URL 的动态部分，作为参数传入视图函数
 @app.route("/user/<name>")
 def user(name):
-    # 返回参数被组装成一个元组, 作为响应对象的参数
-    # 第一个参数是 body，第二个是 status，第三个是 headers
-    response = make_response("<h1>Welcome, %s!</h1>" % name, 404, dict())
-    return response
+    return render_template("user.html", name=name)
 
 
 @app.route("/redirect1")
@@ -33,6 +31,16 @@ def redirect1():
 def redirect2():
     response = make_response("", 302, {"Location": "http://kissg.me"})
     return response
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template("500.html"), 500
 
 
 if __name__ == "__main__":
