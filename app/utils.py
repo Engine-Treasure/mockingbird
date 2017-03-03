d5714550cbd366540ffe3332adbd0dd2ddb7b45d
Engine Treasure <enginechen07@gmail.com>
2017-03-03 18:26:36 +0800

    Async Email, fail in reading config

--- app/utils.py
+++ app/utils.py
@@ -5,6 +5,7 @@ __date__ = "2017-02-23"
 
 import random
 import string
+from threading import Thread
 
 from flask import render_template
 from flask_mail import Message
@@ -26,12 +27,18 @@ def gen_random_str(mode=1, length=8):
         random.SystemRandom().choice(s) for _ in range(length))
 
 
+def send_async_email(app, msg):
+    with app.context():  # Flask-Mail 的 send 函数使用 current_app, 需激活 context
+        mail.send(msg)
+
+
 def send_email(to, subject, template, **kwargs):
     msg = Message(recipients=[to],
                   subject=Config.MOCKINGBIRD_MAIL_SUBJECT_PREFIX + subject,
                   sender=Config.MOCKINGBIRD_MAIL_SENDER)
     msg.body = render_template(template + ".txt", **kwargs)
-    mail.send(msg)
+    thr = Thread(target=send_async_email, args=[app, msg])
+    return thr
 
 
 if __name__ == '__main__':
